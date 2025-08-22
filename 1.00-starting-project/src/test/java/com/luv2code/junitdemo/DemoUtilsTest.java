64f62b58405982a5ca4067bff59a792273c8d86e
Evgenii Troinov <troinof@gmail.com>
2025-08-22 23:25:29 +0200

    main: Added base tests

--- 1.00-starting-project/src/test/java/com/luv2code/junitdemo/DemoUtilsTest.java
+++ 1.00-starting-project/src/test/java/com/luv2code/junitdemo/DemoUtilsTest.java
@@ -1,23 +1,53 @@
 package com.luv2code.junitdemo;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class DemoUtilsTest {
-    @Test
-    void testEqualsAndNotEquals() {
-        DemoUtils demoUtils = new DemoUtils();
+    DemoUtils demoUtils;
+    @BeforeEach
+    void setupBeforeEach() {
+        demoUtils = new DemoUtils();
+    }
 
+    @Test
+    @DisplayName("Should return correct value")
+    void testEqualsAndNotEquals() {
         assertEquals(6, demoUtils.add(2, 4), "2 + 4 must be 6");
         assertNotEquals(8, demoUtils.add(2, 4), "2 + 4 is not 8");
     }
 
     @Test
+    @DisplayName("Should return true")
     void testNullAndNotNull() {
-        DemoUtils demoUtils = new DemoUtils();
-
         assertNull(demoUtils.checkNull(null), "Object should be null");
         assertNotNull(demoUtils.checkNull("My name is"), "Object should not be null");
+    }
 
+    @DisplayName("True or false")
+    @Test
+    void trueOrFalse () {
+        assertTrue(demoUtils.isGreater(2, 1), "This should return true");
+        assertFalse(demoUtils.isGreater(1, 2), "This should return false");
+    }
+
+    @DisplayName("Array Equals")
+    @Test
+    void testArrayEquals() {
+        String[] arr = {"A", "B", "C"};
+        assertArrayEquals(demoUtils.getFirstThreeLettersOfAlphabet(), arr);
+    }
+
+    @DisplayName("Iterable Equals")
+    @Test
+    void testIterableEquals() {
+        List<String> list = List.of("luv", "2", "code");
+
+        assertIterableEquals(demoUtils.getAcademyInList(), list, "Lists should be Equals");
     }
 }
